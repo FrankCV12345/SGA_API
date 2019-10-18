d55fd109e9583043975d8b015a3a9d689c36f960
Your Name <you@example.com>
2019-10-18 12:49:44 -0500

    agrego la base del proyecto

---
+++ src/main/java/com/sga/SGA/SgaApiApplication.java
@@ -0,0 +1,13 @@
+package com.sga.SGA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sga/SGA/SgaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sga.SGA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
